e25335f739ae71f9f03b5cf49f61578b70fd6c6c
Daniel Muller <dmuller720@gmail.com>
2024-03-23 21:46:04 -0400

    fork logic for domain wrapper core

---
+++ src/core/internals/procedureBuilder.ts
@@ -0,0 +1,269 @@
+import type { MiddlewareBuilder, MiddlewareFunction, MiddlewareResult } from "../middleware";
+import { createInputMiddleware, createOutputMiddleware } from "../middleware";
+import type { inferParser, Parser } from "../parser";
+import { AnyProcedure, Procedure, ProcedureParams } from "../procedure";
+import type { AnyRootConfig } from "./config";
+import { getParseFn } from "./getParseFn";
+import { mergeWithoutOverrides } from "./mergeWithoutOverrides";
+import type { DefaultValue, Overwrite, OverwriteKnown, ResolveOptions, UnsetMarker } from "./utils";
+import { middlewareMarker } from "./utils";
+import { MaybePromise, Simplify } from "../util-types";
+import { getTRPCErrorFromUnknown, TRPCError } from "../error";
+
+type CreateProcedureReturnInput<TPrev extends ProcedureParams, TNext extends ProcedureParams> = ProcedureBuilder<{
+  _config: TPrev["_config"];
+  _ctx_out: Overwrite<TPrev["_ctx_out"], TNext["_ctx_out"]>;
+  _input_in: TPrev["_input_in"];
+  _input_out: UnsetMarker extends TNext["_input_out"]
+    ? TPrev["_input_out"]
+    : Overwrite<TPrev["_input_out"], TNext["_input_out"]>;
+  _output_in: DefaultValue<TNext["_output_in"], TPrev["_output_in"]>;
+  _output_out: DefaultValue<TNext["_output_out"], TPrev["_output_out"]>;
+}>;
+
+/**
+ * @internal
+ */
+export interface BuildProcedure<TParams extends ProcedureParams, TOutput>
+  extends Procedure<
+    UnsetMarker extends TParams["_output_out"]
+      ? OverwriteKnown<
+          TParams,
+          {
+            _output_in: TOutput;
+            _output_out: TOutput;
+          }
+        >
+      : TParams
+  > {}
+
+type OverwriteIfDefined<TType, TWith> = UnsetMarker extends TType ? TWith : Simplify<TType & TWith>;
+
+type ErrorMessage<TMessage extends string> = TMessage;
+
+export type ProcedureBuilderDef<_TParams extends ProcedureParams> = {
+  inputs: Parser[];
+  output?: Parser;
+  resolver?: ProcedureBuilderResolver;
+  middlewares: ProcedureBuilderMiddleware[];
+};
+
+export type AnyProcedureBuilderDef = ProcedureBuilderDef<any>;
+
+export interface ProcedureBuilder<TParams extends ProcedureParams> {
+  /**
+   * Add an input parser to the procedure.
+   */
+  input<$Parser extends Parser>(
+    schema: TParams["_input_out"] extends UnsetMarker
+      ? $Parser
+      : inferParser<$Parser>["out"] extends Record<string, unknown> | undefined
+        ? TParams["_input_out"] extends Record<string, unknown> | undefined
+          ? undefined extends inferParser<$Parser>["out"] // if current is optional the previous must be too
+            ? undefined extends TParams["_input_out"]
+              ? $Parser
+              : ErrorMessage<"Cannot chain an optional parser to a required parser">
+            : $Parser
+          : ErrorMessage<"All input parsers did not resolve to an object">
+        : ErrorMessage<"All input parsers did not resolve to an object">
+  ): ProcedureBuilder<{
+    _config: TParams["_config"];
+    _ctx_out: TParams["_ctx_out"];
+    _input_in: OverwriteIfDefined<TParams["_input_in"], inferParser<$Parser>["in"]>;
+    _input_out: OverwriteIfDefined<TParams["_input_out"], inferParser<$Parser>["out"]>;
+
+    _output_in: TParams["_output_in"];
+    _output_out: TParams["_output_out"];
+  }>;
+  /**
+   * Add an output parser to the procedure.
+   */
+  output<$Parser extends Parser>(
+    schema: $Parser
+  ): ProcedureBuilder<{
+    _config: TParams["_config"];
+    _ctx_out: TParams["_ctx_out"];
+    _input_in: TParams["_input_in"];
+    _input_out: TParams["_input_out"];
+    _output_in: inferParser<$Parser>["in"];
+    _output_out: inferParser<$Parser>["out"];
+  }>;
+  /**
+   * Add a middleware to the procedure.
+   */
+  use<$Params extends ProcedureParams>(
+    fn: MiddlewareBuilder<TParams, $Params> | MiddlewareFunction<TParams, $Params>
+  ): CreateProcedureReturnInput<TParams, $Params>;
+  /**
+   * Resolve procedure
+   */
+  resolve<$Output>(
+    resolver: (opts: ResolveOptions<TParams>) => MaybePromise<DefaultValue<TParams["_output_in"], $Output>>
+  ): BuildProcedure<TParams, $Output>;
+  /**
+   * @internal
+   */
+  _def: ProcedureBuilderDef<TParams>;
+}
+
+type AnyProcedureBuilder = ProcedureBuilder<any>;
+
+export type ProcedureBuilderMiddleware = MiddlewareFunction<any, any>;
+
+export type ProcedureBuilderResolver = (opts: ResolveOptions<any>) => Promise<unknown>;
+
+function createNewBuilder(def1: AnyProcedureBuilderDef, def2: Partial<AnyProcedureBuilderDef>) {
+  const { middlewares = [], inputs, ...rest } = def2;
+
+  // TODO: maybe have a fn here to warn about calls
+  return createBuilder({
+    ...mergeWithoutOverrides(def1, rest),
+    inputs: [...def1.inputs, ...(inputs ?? [])],
+    middlewares: [...def1.middlewares, ...middlewares],
+  });
+}
+
+export function createBuilder<TConfig extends AnyRootConfig>(
+  initDef: Partial<AnyProcedureBuilderDef> = {}
+): ProcedureBuilder<{
+  _config: TConfig;
+  _ctx_out: TConfig["$types"]["ctx"];
+  _input_in: UnsetMarker;
+  _input_out: UnsetMarker;
+  _output_in: UnsetMarker;
+  _output_out: UnsetMarker;
+}> {
+  const _def: AnyProcedureBuilderDef = {
+    inputs: [],
+    middlewares: [],
+    ...initDef,
+  };
+
+  return {
+    _def,
+    input(input) {
+      const parser = getParseFn(input);
+      return createNewBuilder(_def, {
+        inputs: [input],
+        middlewares: [createInputMiddleware(parser)],
+      }) as AnyProcedureBuilder;
+    },
+    output(output: Parser) {
+      const parseOutput = getParseFn(output);
+      return createNewBuilder(_def, {
+        output,
+        middlewares: [createOutputMiddleware(parseOutput)],
+      }) as AnyProcedureBuilder;
+    },
+    use(middlewareBuilderOrFn) {
+      // Distinguish between a middleware builder and a middleware function
+      const middlewares =
+        "_middlewares" in middlewareBuilderOrFn ? middlewareBuilderOrFn._middlewares : [middlewareBuilderOrFn];
+
+      return createNewBuilder(_def, {
+        middlewares: middlewares as ProcedureBuilderMiddleware[],
+      }) as AnyProcedureBuilder;
+    },
+    resolve(resolver) {
+      return createResolver(_def, resolver) as AnyProcedure;
+    },
+  };
+}
+
+function createResolver(_def: AnyProcedureBuilderDef, resolver: (opts: ResolveOptions<any>) => MaybePromise<any>) {
+  const finalBuilder = createNewBuilder(_def, {
+    resolver,
+    middlewares: [
+      async function resolveMiddleware(opts) {
+        const data = await resolver(opts);
+        return {
+          marker: middlewareMarker,
+          ok: true,
+          data,
+          ctx: opts.ctx,
+        } as const;
+      },
+    ],
+  });
+
+  return createProcedureCaller(finalBuilder._def);
+}
+
+/**
+ * @internal
+ */
+export interface ProcedureCallOptions {
+  ctx: unknown;
+  rawInput: unknown;
+  input?: unknown;
+  path: string;
+}
+
+function createProcedureCaller(_def: AnyProcedureBuilderDef): AnyProcedure {
+  const procedure = async function resolve(opts: ProcedureCallOptions) {
+    // run the middlewares recursively with the resolver as the last one
+    const callRecursive = async (
+      callOpts: {
+        ctx: any;
+        index: number;
+        input?: unknown;
+        rawInput?: unknown;
+      } = {
+        index: 0,
+        ctx: opts.ctx,
+      }
+    ): Promise<MiddlewareResult<any>> => {
+      try {
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        const middleware = _def.middlewares[callOpts.index]!;
+        const result = await middleware({
+          ctx: callOpts.ctx,
+          path: opts.path,
+          rawInput: callOpts.rawInput ?? opts.rawInput,
+          input: callOpts.input,
+          next(_nextOpts?: any) {
+            const nextOpts = _nextOpts as
+              | {
+                  ctx?: Record<string, unknown>;
+                  input?: unknown;
+                  rawInput?: unknown;
+                }
+              | undefined;
+
+            return callRecursive({
+              index: callOpts.index + 1,
+              ctx: nextOpts && "ctx" in nextOpts ? { ...callOpts.ctx, ...nextOpts.ctx } : callOpts.ctx,
+              input: nextOpts && "input" in nextOpts ? nextOpts.input : callOpts.input,
+              rawInput: nextOpts && "rawInput" in nextOpts ? nextOpts.rawInput : callOpts.rawInput,
+            });
+          },
+        });
+        return result;
+      } catch (cause) {
+        return {
+          ok: false,
+          error: getTRPCErrorFromUnknown(cause),
+          marker: middlewareMarker,
+        };
+      }
+    };
+
+    // there's always at least one "next" since we wrap this.resolver in a middleware
+    const result = await callRecursive();
+
+    if (!result) {
+      throw new TRPCError({
+        code: "INTERNAL_SERVER_ERROR",
+        message: "No result from middlewares - did you forget to `return next()`?",
+      });
+    }
+    if (!result.ok) {
+      // re-throw original error
+      throw result.error;
+    }
+    return result.data;
+  };
+  procedure._def = _def;
+
+  return procedure as AnyProcedure;
+}
---
+++ src/core/procedure.ts
@@ -0,0 +1,75 @@
+import type { AnyRootConfig } from "./internals/config";
+import type { ProcedureBuilderDef, ProcedureCallOptions } from "./internals/procedureBuilder";
+import type { UnsetMarker } from "./internals/utils";
+
+type ClientContext = Record<string, unknown>;
+
+/**
+ * @internal
+ */
+export interface ProcedureOptions {
+  /**
+   * Client-side context
+   */
+  context?: ClientContext;
+  signal?: AbortSignal;
+}
+
+/**
+ * FIXME: this should only take 1 generic argument instead of a list
+ * @internal
+ */
+export interface ProcedureParams<
+  TConfig extends AnyRootConfig = AnyRootConfig,
+  TContextOut = unknown,
+  TInputIn = unknown,
+  TInputOut = unknown,
+  TOutputIn = unknown,
+  TOutputOut = unknown,
+> {
+  _config: TConfig;
+  /**
+   * @internal
+   */
+  _ctx_out: TContextOut;
+  /**
+   * @internal
+   */
+  _input_in: TInputIn;
+  /**
+   * @internal
+   */
+  _input_out: TInputOut;
+  /**
+   * @internal
+   */
+  _output_in: TOutputIn;
+  /**
+   * @internal
+   */
+  _output_out: TOutputOut;
+}
+
+/**
+ * @internal
+ */
+export type ProcedureArgs<TParams extends ProcedureParams> = TParams["_input_in"] extends UnsetMarker
+  ? [input?: undefined | void, opts?: ProcedureOptions]
+  : undefined extends TParams["_input_in"]
+    ? [input?: TParams["_input_in"] | void, opts?: ProcedureOptions]
+    : [input: TParams["_input_in"], opts?: ProcedureOptions];
+
+/**
+ *
+ * @internal
+ */
+export interface Procedure<TParams extends ProcedureParams> {
+  _def: ProcedureBuilderDef<TParams> & TParams;
+  _procedure: true;
+  /**
+   * @internal
+   */
+  (opts: ProcedureCallOptions): Promise<unknown>;
+}
+
+export type AnyProcedure = Procedure<any>;
